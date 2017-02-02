c00f4520fc6ab3f4c0d23cc8d519611da119f94f
x090371 <Daniel.Moll@klm.com>
2017-02-02 14:15:04 +0100

    updated error message

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -57,8 +57,8 @@ exports.signup = function (req, res) {
 exports.signin = function (req, res, next) {
   passport.authenticate('local', function (err, user, info) {
     if (err || !user) {
-      console.log('Sign in failed for user: ' + user.username + " error: " + err);
-      winston.info('Sign in failed for user: ' + user.username + " error: " + err);
+      console.log('Sign in failed for user: ' + user);
+      winston.info('Sign in failed for user: ' + user);
       res.status(400).send(info);
     } else {
       // Remove sensitive data before login
